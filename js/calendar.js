046bc842860b364f71930a86c757546912338bbf
BAZbIL <45766314+BAZbIL@users.noreply.github.com>
2019-08-07 12:42:28 +0300

    Update project for Success Craft

--- js/calendar.js
+++ js/calendar.js
@@ -1,27 +1,27 @@
 'use strict';
 
-(function () {
+// INIT DATA
 
-  // INIT DATA
-
-  const params = {
+const params = {
     dom: document.getElementById('calendar'),
     year: null,
     month: null,
     onClickDate: function (date) {
 
     }
-  };
+};
 
-  let $wrapper = null;
+let $wrapper = null;
 
-  // INIT
+let prevTargetBlock;
 
-  init();
+// INIT
 
-  // CORE
+init();
 
-  function getDateData(year, month) {
+// CORE
+
+function getDateData(year, month) {
 
     let ret = [];
 
@@ -45,72 +45,72 @@
 
     for (let i = 0; i < 7 * 6; i++) {
 
-      let date = i + 1 - preMonthDayCount;
+        let date = i + 1 - preMonthDayCount;
 
-      let showDate = date;
+        let showDate = date;
 
-      let thisMonth = params.month;
+        let thisMonth = params.month;
 
-      if (date <= 0) {
+        if (date <= 0) {
 
-        thisMonth = params.month - 1;
-        showDate = lastDateOfLastMonth + date;
-        styleCls = 'gray';
+            thisMonth = params.month - 1;
+            showDate = lastDateOfLastMonth + date;
+            styleCls = 'gray';
 
 
-      } else if (date > lastDate) {
+        } else if (date > lastDate) {
 
-        thisMonth = params.month + 1;
-        showDate = showDate - lastDate;
-        styleCls = 'gray';
-
-      } else {
-
-        today = new Date();
-
-        if (showDate === today.getDate() && thisMonth === today.getMonth() + 1) {
-
-          styleCls = 'normal current';
+            thisMonth = params.month + 1;
+            showDate = showDate - lastDate;
+            styleCls = 'gray';
 
         } else {
 
-          styleCls = 'normal';
+            today = new Date();
+
+            if (showDate === today.getDate() && thisMonth === today.getMonth() + 1) {
+
+                styleCls = 'normal current';
+
+            } else {
+
+                styleCls = 'normal';
+
+            }
+
 
         }
 
+        if (thisMonth === 13) {
 
-      }
+            thisMonth = 1;
 
-      if (thisMonth === 13) {
+        }
 
-        thisMonth = 1;
+        if (thisMonth === 0) {
 
-      }
+            thisMonth = 12;
 
-      if (thisMonth === 0) {
+        }
 
-        thisMonth = 12;
-
-      }
-
-      ret.push({
-        month: thisMonth,
-        date: date,
-        showDate: showDate,
-        styleCls: styleCls
-      });
+        ret.push({
+            month: thisMonth,
+            date: date,
+            showDate: showDate,
+            styleCls: styleCls
+        });
 
     }
 
     return {
-      year: year,
-      month: month,
-      date: ret
+        year: year,
+        month: month,
+        date: ret
     };
 
-  }
+}
 
-  function generateDom() {
+function generateDom() {
 
     let dayWords = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 
@@ -119,97 +119,99 @@
     let monthData = getDateData(params.year, params.month);
 
     let html =
-      '<div class="wrapper">' +
-      '<div class="header">' +
-      '<button class="prev-date-btn arrow">&#9664;</button>' +
-      '<span class="title">' + enMonthsWords[monthData.month - 1] + ' ' + monthData.year + '</span>' +
-      '<button class="next-date-btn arrow">&#9654;</button>' +
-      '<button class="now-date-btn">Сегодня</button>' +
-      '</div>' +
-      '<div class="body">' +
-      '<table>' +
-      '<thead>' +
-      '<tr>' +
-      '</tr>' +
-      '</thead>' +
-      '<tbody>';
+        '<div class="wrapper">' +
+        '<div class="header">' +
+        '<button class="prev-date-btn arrow">&#9664;</button>' +
+        '<span class="title">' + enMonthsWords[monthData.month - 1] + ' ' + monthData.year + '</span>' +
+        '<button class="next-date-btn arrow">&#9654;</button>' +
+        '<button class="now-date-btn">Сегодня</button>' +
+        '</div>' +
+        '<div class="body">' +
+        '<table>' +
+        '<thead>' +
+        '<tr>' +
+        '</tr>' +
+        '</thead>' +
+        '<tbody>';
 
     for (let i = 0; i < monthData.date.length; i++) {
 
-      let dayCount = i % 7;
+        let dayCount = i % 7;
 
-      let viewDate = monthData.date[i].showDate;
+        let viewDate = monthData.date[i].showDate;
 
-      if (i <= 6) {
+        if (i <= 6) {
 
-        viewDate = `${dayWords[dayCount]}, ${monthData.date[i].showDate}`;
+            viewDate = `${dayWords[dayCount]}, ${monthData.date[i].showDate}`;
 
-      }
+        }
 
-      if (i % 7 === 0) {
+        if (i % 7 === 0) {
 
-        html += '<tr>';
+            html += '<tr>';
 
-      }
+        }
 
-      html += '<td class="' + monthData.date[i].styleCls + '" data-date="' + monthData.year + '/' + monthData.month + '/' + monthData.date[i].showDate + '">' + viewDate + '</td>';
+        html += '<td class="' + monthData.date[i].styleCls + '" data-date="' + monthData.year + '/' + monthData.month + '/' + monthData.date[i].showDate + '">' + viewDate + '</td>';
 
-      if (i % 7 === 6) {
+        if (i % 7 === 6) {
 
-        html += '</tr>';
+            html += '</tr>';
 
-      }
+        }
 
     }
 
     html += '</tbody>' +
-      '</table>' +
-      '</div>' +
-      '</div>';
+        '</table>' +
+        '</div>' +
+        '</div>';
 
 
     return html;
 
-  }
+}
 
-  function render(direction, params) {
+function render(direction, params) {
 
     if (direction === 'prev') {
 
-      params.month--;
+        params.month--;
 
-      if (params.month === 0) {
+        if (params.month === 0) {
 
-        params.month = 12;
-        params.year--;
+            params.month = 12;
+            params.year--;
 
-      }
+        }
 
     }
 
     if (direction === 'next') {
 
-      params.month++;
+        params.month++;
 
-      if (params.month === 13) {
+        if (params.month === 13) {
 
-        params.month = 1;
-        params.year++;
+            params.month = 1;
+            params.year++;
 
-      }
+        }
 
     }
 
     $wrapper.innerHTML = generateDom(params.year, params.month);
 
-  }
+}
 
-  function initData() {
+function initData() {
     params.year = new Date().getFullYear();
     params.month = new Date().getMonth() + 1;
-  }
+}
 
-  function init() {
+function init() {
+
+    let $body = document.querySelector('body');
 
     $wrapper = params.dom;
 
@@ -219,30 +221,125 @@
 
     $wrapper.addEventListener('click', function (e) {
 
-      let $target = e.target;
+        let $target = e.target;
 
-      if ($target.classList.contains('prev-date-btn')) {
+        if ($target.classList.contains('prev-date-btn')) {
 
-        render('prev', params);
+            render('prev', params);
 
-      }
+        }
 
-      if ($target.classList.contains('next-date-btn')) {
+        if ($target.classList.contains('next-date-btn')) {
 
-        render('next', params);
+            render('next', params);
 
-      }
+        }
 
-      if ($target.classList.contains('now-date-btn')) {
+        if ($target.classList.contains('now-date-btn')) {
 
-        initData();
+            initData();
 
-        render('', params);
+            render('', params);
 
-      }
+        }
+
+        if ($target.tagName === 'TD') {
+
+            note($target);
+            addNote__quick($target);
+
+        }
+
+        if ($target.tagName === 'TD2') {
+
+            note_2($target);
+            addNote__quick_2($target);
+
+        }
 
     }, false);
 
-  }
+    $body.addEventListener('click', function (e) {
 
-})();
+        console.log(document.getElementById('add-note__quick'));
+
+    }, false);
+
+    $body.addEventListener('click', function (e) {
+
+        console.log(document.getElementById('add-note__quick_2'));
+
+    }, false);
+
+}
+
+function note(node) {
+    if (prevTargetBlock) {
+        prevTargetBlock.classList.remove('note');
+    }
+    prevTargetBlock = node;
+    prevTargetBlock.classList.add('note');
+}
+
+function addNote__quick(node) {
+
+    closeForm();
+
+    node.innerHTML =
+        '<form id="add-note__quick">' +
+        '<i id="close2" onclick="closeForm()">&times;</i>' +
+        '<input type="text"  class="field" placeholder="Событие">' +
+        '<input type="text"  class="field" placeholder="День, месяц, год">' +
+        '<input type="text"  class="field" placeholder="Участники">' +
+        '<input type="text"  class="field op" placeholder="Описание">' +
+        '<button type="submit">Готово</button>' +
+        '<button type="submit">Удалить</button>' +
+        '</form>';
+
+}
+
+function closeForm() {
+
+    let $form = document.getElementById('add-note__quick');
+
+    if ($form) {
+        $form.remove()
+    }
+
+}
+
+
+//Add button
+
+function note_2(node_2) {
+    if (prevTargetBlock) {
+        prevTargetBlock.classList.remove('note_2');
+    }
+    prevTargetBlock = node_2;
+    prevTargetBlock.classList.add('note_2');
+}
+
+function addNote__quick_2(node_2) {
+
+    closeForm_2();
+
+    node_2.innerHTML =
+    '<form id="add-note__quick_2">' +
+    '<i id="close3" onclick="closeForm()">&times;</i>' +
+    '<input type="text"  class="field" placeholder="5 марта, 14:00, День рождения">' +
+    '<button type="submit">Создать</button>' +
+    '</form>'
+    ;
+
+
+}
+
+function closeForm_2() {
+
+    let $form = document.getElementById('add-note__quick_2');
+
+    if ($form) {
+        $form.remove()
+    }
+
+}
